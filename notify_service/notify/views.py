145e9b6576f302e5455f3a16a354491a1863913b
E.Moiseev <sk831it3@mail.ru>
2023-04-24 15:16:59 +0300

    create sync api

--- notify_service/notify/views.py
+++ notify_service/notify/views.py
@@ -1,22 +1,20 @@
-from django.http import JsonResponse, HttpRequest
-import abc
+"""Модуль с реализациями ручек  API Django."""
 
-from notify.models import NotifyType
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+
+from .utils import EventValidator
 from notify.tasks import treatment_api_data
 
 
-class EventValidator(abc.ABC):
-    def validate(self, request: HttpRequest) -> bool:
-        if notify_type := NotifyType.objects.filter(slug__iexact=request.POST.get("event_type")).afirst():
-            return True
-        else:
-            return False
+@csrf_exempt
+def create_notify(request: HttpRequest):
+    """ Функция, принимающая данные для отправки уведомления и передающая их в `Celery task`."""
 
-
-async def create_notify(request: HttpRequest):
     validator = EventValidator()
+
     if validator.validate(request):
-        treatment_api_data.delay(data=request.POST)
-        return JsonResponse(data={}, status=201)
-    else:
-        return JsonResponse(data={}, status=400)
+        # treatment_api_data.delay(data=request.GET)
+        return HttpResponse('OK')
+
+    return HttpResponseBadRequest('BadRequest')
