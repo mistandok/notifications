f82854948f41739cc902d0dc067d48afd82fc9dd
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2023-04-25 23:46:59 +0300

    all services work together

--- notify_service/start-django.sh
+++ notify_service/start-django.sh
@@ -22,6 +22,7 @@ fi
 
 chown www-data:www-data /var/log
 python manage.py migrate
+python manage.py createsuperuser_if_not_exists --user=admin --password=admin
 python manage.py collectstatic --no-input
 
 gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker -w 3  --bind 0.0.0.0:8000
