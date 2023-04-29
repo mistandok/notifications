94d2cef467c29cbef49d0e6c5c6e07679caf868e
Bexram <bexram33@mail.ru>
2023-04-29 13:57:40 +0300

    массовая рассылка для провайдера

--- notify_service/notify/providers/provider.py
+++ notify_service/notify/providers/provider.py
@@ -2,6 +2,7 @@ import abc
 from functools import lru_cache
 
 from django.conf import settings
+from django.core.mail import get_connection, EmailMultiAlternatives
 
 from notify.models import Notify
 from django.core.mail import send_mail
@@ -36,20 +37,58 @@ class MailProvider(SenderProvider):
 
     provider_name = "mail"
 
-    def send(self, data: dict, notify: Notify):
+    def send(self, data: list, notify: Notify):
+        """
+        Формирует шаблоны для отправки и передает в отправку.
+        @param data: Данные для отправки формата
+        [
+          {'email': 'bexram33@mail.ru',
+           'surname': 'Ilya',
+           ...},
+        ...]
+
+        @param notify: экземляр уведомления
+        """
         template = notify.notify_type.template.html
-        for key in data.keys():
-            if data.get(key):
-                template = template.replace(key, data.get(key))
-        plain_message = strip_tags(template)
-        send_mail(
-            subject=data.get("subject"),
-            message=plain_message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[data.get("email")],
-            fail_silently=False,
-            html_message=template,
+        self.send_mass_html_mail(
+            data, notify.notify_type.name, template
         )
+
+    def send_mass_html_mail(
+        self,
+        datatuple,
+        subject: str,
+        template: str,
+    ):
+        """
+        Устанавливает соединенеие с почтовым сервером и отправляет пачку сообщений.
+        @param datatuple: данные для отправки и рендера формата
+        [
+          {'email': 'bexram33@mail.ru',
+           'surname': 'Ilya',
+           ...},
+           ...]
+        @param subject: Тема для сообщения
+        @param template: Строка шаблона для ренедера
+        @return:
+        """
+        connection = get_connection(
+            username=settings.EMAIL_HOST_USER,
+            password=settings.EMAIL_HOST_PASSWORD,
+            fail_silently=True,
+        )
+        messages = []
+        for user in datatuple:
+            for key in user.keys():
+                if user.get(key):
+                    template = template.replace(key, user.get(key))
+            plain_message = strip_tags(template)
+            message = EmailMultiAlternatives(
+                subject, plain_message, settings.EMAIL_HOST_USER, user.get("email")
+            )
+            message.attach_alternative(template, "text/html")
+            messages.append(message)
+        return connection.send_messages(messages)
 
 
 @lru_cache()
