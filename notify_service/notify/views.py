ef6d11595af8cc899fdd9020c3cc1aec02df62aa
E.Moiseev <sk831it3@mail.ru>
2023-04-29 21:34:56 +0300

    заменил на namedtuple

--- notify_service/notify/views.py
+++ notify_service/notify/views.py
@@ -4,16 +4,16 @@ from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpR
 
 from .validators import EventValidator
 from notify.tasks import treatment_api_data
-from .constants import EventValidatorResponse
 
 
 async def create_notify(request: HttpRequest) -> HttpResponseBase:
     """ Функция, принимающая данные для отправки уведомления и передающая их в `Celery task`."""
 
     validator = EventValidator()
+    validate_result = await validator.validate(request)
 
-    if (validate_result := await validator.validate(request)) == EventValidatorResponse.OK:
+    if validate_result.status:
         treatment_api_data.delay(data=request.GET)
-        return HttpResponse(validate_result)
+        return HttpResponse(validate_result.detail)
 
-    return HttpResponseBadRequest(validate_result)
+    return HttpResponseBadRequest(validate_result.detail)
